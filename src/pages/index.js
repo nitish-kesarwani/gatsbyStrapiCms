48e9c1af9d85fb6e26d0f9b2accb34aafff673c5
Nitish Kesarwani <Nitish.Kesarwani@rsystems.com>
2021-10-01 00:11:00 +0530

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,53 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { StaticQuery, graphql } from "gatsby"
+// import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+// import Reactmarkdown from "react-markdown"
 
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
+  <StaticQuery
+    query = {graphql`
+      query getBlogs {
+        allStrapiBlogs {
+          nodes {
+            id
+            title
+            description
+            publishedDate
+            content
+            image {
+              id
+              url
+            }
+          }
+        }
+      }    
+    `}
+    render = {data => (
+      <Layout>
+        <Seo title="Home" />
+        <h1>Blogs</h1>
+        {
+          data?.allStrapiBlogs?.nodes?.map(blog => (
+            <div key={blog?.id} style={{marginBottom:40}}>
+              <h3>{blog?.title}</h3>
+              <div>{blog?.description}</div>
+              <div
+                className="blog-post-content"
+                dangerouslySetInnerHTML={{ __html: blog?.content }}
+              />
+              {/* <Reactmarkdown source={blog?.content} /> */}
+              {/* <div>{blog?.content}</div> */}
+            </div>
+          ))
+        }
+      </Layout>
+    )}
+  />
+
+
 )
 
 export default IndexPage
